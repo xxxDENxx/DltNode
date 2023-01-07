e6907f397f4730dbc02fd809383166a4f50e05fc
Denis <Denis@DENPC>
2023-01-07 12:28:14 +1000

    sign

--- DltNode/DltNode.Blockchain/Transaction.cs
+++ DltNode/DltNode.Blockchain/Transaction.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using DltNode.Hash;
 using System.Text;
+using System.Security.Cryptography;
+
 namespace DltNode.Blockchain
 {
 	public class Transaction
 	{
 		public readonly String info;
-		public Transaction(string info)
+
+		public Byte[] signature;
+
+		public Byte[] from; //pbKey from sender
+
+		public Transaction(String info)
         {
 			this.info = info;
+        }
+
+		public void Sign(RSAParameters parameters)
+        {
+			var hash = this.GetHash();
+			using (RSA rsa = RSA.Create())
+            {
+				rsa.ImportParameters(parameters);
+				RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
+				rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+				signature = rsaFormatter.CreateSignature(hash);
+            }
+        }
+
+		public Boolean VerifySignature(Byte[] publicKey)
+        {
+			return false;
         }
 
 		public Byte[] GetHash() => PureHash.computeHash(Encoding.UTF8.GetBytes(info));
